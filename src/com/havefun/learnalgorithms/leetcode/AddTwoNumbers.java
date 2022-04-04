12e891759594600e04e4413f6d7996aab56472a2
WinterSweett <hongmeizhou@pateo.com.cn>
2022-04-04 11:36:47 +0800

    0404

--- src/com/havefun/learnalgorithms/leetcode/AddTwoNumbers.java
+++ src/com/havefun/learnalgorithms/leetcode/AddTwoNumbers.java
@@ -1,3 +1,8 @@
+package com.havefun.learnalgorithms.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+/*
 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
 
 请你将两个数相加，并以相同形式返回一个表示和的链表。
@@ -27,19 +32,12 @@
 每个链表中的节点数在范围 [1, 100] 内
 0 <= Node.val <= 9
 题目数据保证列表表示的数字不含前导零
+*/
 
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
+
+
+public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode pre = new ListNode(0);
         ListNode cur = pre;
@@ -54,7 +52,7 @@ class Solution {
 
             cur.next = new ListNode(sum);
             cur = cur.next;
-            
+
             if (l1 != null ) {
                 l1 = l1.next;
             }
@@ -62,10 +60,18 @@ class Solution {
                 l2 = l2.next;
             }
         }
-        
+
         if (carry != 0) {
             cur.next = new ListNode(carry);
         }
         return pre.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
---
+++ src/com/havefun/learnalgorithms/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,82 @@
+package com.havefun.learnalgorithms.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author : WinterSweett
+ * @time : {DATE}
+ */
+
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= s.length <= 5 * 104
+ * s 由英文字母、数字、符号和空格组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class LengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0, size = s.length(), left = 0;
+        for (int i = 0; i < size; i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                left = Math.max(left, map.get(ch) + 1);
+            }
+            map.put(ch, i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Set<Character> set = new HashSet<>();
+        int rk = -1, ans = 1, size = s.length();
+
+        for (int i = 0; i < size; i++) {
+            char ch = s.charAt(i);
+            if (i != 0) {
+                set.remove(ch);
+            }
+            while (rk + 1 < size && !set.contains(s.charAt(rk + 1))) {
+                set.add(s.charAt(rk + 1));
+                rk++;
+            }
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+}
